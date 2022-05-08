d51e3c7fed95a0762dac0420c4767f36066edeef
Jeffrey Tse <jeffreytse.mail@gmail.com>
2022-05-08 14:02:01 +0800

    feat: support to restore mtime of all files (#40)
    Restore the modification time (mtime) of all files in work tree, based
    on the date of the most recent commit that modified the file.

--- entrypoint.sh
+++ entrypoint.sh
@@ -49,6 +49,10 @@ ${SCRIPT_DIR}/script/init_environment.sh
 
 cd ${JEKYLL_SRC}
 
+# Restore modification time (mtime) of git files
+echo "Restore modification time of all git files"
+${SCRIPT_DIR}/script/restore_mtime.sh
+
 # Check and execute pre_build_commands commands
 if [[ ${PRE_BUILD_COMMANDS} ]]; then
   echo "Executing pre-build commands"
