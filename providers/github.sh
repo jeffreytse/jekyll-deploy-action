94e5a0079a0152090650147c164c84335f2037e6
Jeffrey Tse <jeffreytse.mail@gmail.com>
2023-02-23 10:46:56 +0800

    feat: using 'main' branch for the git inital branch

--- providers/github.sh
+++ providers/github.sh
@@ -17,12 +17,13 @@ echo "Deploying to ${REPOSITORY} on branch ${BRANCH}"
 echo "Deploying to https://${ACTOR}:${TOKEN}@github.com/${REPOSITORY}.git"
 
 REMOTE_REPO="https://${ACTOR}:${TOKEN}@github.com/${REPOSITORY}.git" && \
+  git config --global init.defaultBranch main && \
   git init && \
   git config user.name "${ACTOR}" && \
   git config user.email "${ACTOR}@users.noreply.github.com" && \
   git add . && \
   git commit -m "jekyll build from Action ${GITHUB_SHA}" && \
-  git push --force $REMOTE_REPO master:$BRANCH && \
+  git push --force $REMOTE_REPO main:$BRANCH && \
   fuser -k .git || rm -rf .git && \
   cd ..
 
