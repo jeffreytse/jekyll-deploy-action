c3fd7b7fc6117d65af4c633cbfe634269bdbeee8
Jeffrey Tse <jeffreytse.mail@gmail.com>
2025-11-09 00:45:02 +0800

    refactor: better initialization script

--- script/init_environment.sh
+++ script/init_environment.sh
@@ -1,53 +1,63 @@
 #!/bin/bash
 
-# Generate a default secret key
-# To prevent archlinux-keyring from no secret key available to sign with
-pacman-key --init
+pre-flight() {
+  # Generate a default secret key
+  # To prevent archlinux-keyring from no secret key available to sign with
+  pacman-key --init
 
-# Update packages database
-pacman -Syu --noconfirm
+  # Update packages database
+  pacman -Syu --noconfirm
 
-# Installing git package
-pacman -S --noconfirm git
+  # Installing git package
+  pacman -S --noconfirm git
 
-# Installing openssh package
-if [[ -n "${SSH_PRIVATE_KEY}" ]]; then
-  pacman -S --noconfirm openssh
-fi
+  # Installing ca-certificates package to avoid SSL issues
+  pacman -S --noconfirm ca-certificates
+  update-ca-trust
 
-# Install asdf-vm for tools' version management
-ASDF_HOME=${WORKING_DIR}/.asdf
+  # Installing openssh package
+  if [[ -n "${SSH_PRIVATE_KEY}" ]]; then
+    pacman -S --noconfirm openssh
+  fi
 
-if [[ ! -f "${ASDF_HOME}/asdf.sh" ]]; then
-  git clone https://github.com/asdf-vm/asdf.git \
-      ${ASDF_HOME} --branch v0.14.0
-fi
+  # Install asdf-vm for tools' version management
+  ASDF_HOME=${WORKING_DIR}/.asdf
 
-source ${ASDF_HOME}/asdf.sh
+  if [[ ! -f "${ASDF_HOME}/asdf.sh" ]]; then
+    git clone https://github.com/asdf-vm/asdf.git \
+        ${ASDF_HOME} --branch v0.14.0
+  fi
 
-# Fix invalid cache to asdf tools' installation
-ln -s ${ASDF_HOME} ${HOME}/.asdf
+  source ${ASDF_HOME}/asdf.sh
 
-# Install ruby environment
-pacman -S --noconfirm libyaml
+  # Fix invalid cache to asdf tools' installation
+  ln -s ${ASDF_HOME} ${HOME}/.asdf
 
-# Manually specify the language version for C compilation for ruby-build
-# to avoid error when building ruby and building ruby extensions.
-# C23 by default in GCC15: https://gcc.gnu.org/gcc-15/changes.html#c
-export RUBY_CFLAGS="-std=gnu17"
+  # Install dependencies of Ruby compilation
+  pacman -S --noconfirm libyaml
 
-if ! asdf list ruby ${RUBY_VER} &>/dev/null; then
-  # Clean up ruby environments avoiding unnecessary cache
-  rm -rf ${ASDF_HOME}/installs/ruby
-  asdf plugin add ruby
-  asdf install ruby ${RUBY_VER}
-fi
+  # This is a temporary workaround
+  # See https://github.com/actions/checkout/issues/766
+  git config --global --add safe.directory "*"
+}
 
-asdf global ruby ${RUBY_VER}
+init_ruby() {
+  # Manually specify the language version for C compilation for ruby-build
+  # to avoid error when building ruby and building ruby extensions.
+  # C23 by default in GCC15: https://gcc.gnu.org/gcc-15/changes.html#c
+  export RUBY_CFLAGS="-std=gnu17"
 
-# debug
-ruby -v && bundle version
+  if ! asdf list ruby ${RUBY_VER} &>/dev/null; then
+    # Clean up ruby environments avoiding unnecessary cache
+    rm -rf ${ASDF_HOME}/installs/ruby
+    asdf plugin add ruby
+    asdf install ruby ${RUBY_VER}
+  fi
 
-# This is a temporary workaround
-# See https://github.com/actions/checkout/issues/766
-git config --global --add safe.directory "*"
+  asdf global ruby ${RUBY_VER}
+
+  # debug
+  ruby -v && bundle version
+}
+
+pre-flight && init_ruby
